3bd229e7839d2f0732495c5e699be9e3d5ad583d
Jovan Lomic <lavzd13@gmail.com>
2024-03-27 18:24:30 +0100

    Added daily exercise in 75

--- 75/flowers.py
+++ 75/flowers.py
@@ -2,8 +2,26 @@ from typing import List
 
 class Solution:
 	def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-		if len(flowerbed) % 2 == 0 and n % 2 == 0 and len(flowerbed) - n >= 4:
-			return True
-		elif len(flowerbed) % 2 != 0 and n % 2 != 0 and len(flowerbed) - n >= 4:
-			return True
-		return False
+		i = 0
+		s = len(flowerbed)
+		while i < s:
+			if flowerbed[i] == 0:
+				next = 0
+				if i < s - 1:
+					next = flowerbed[i + 1]
+				if next == 0:
+					n-=1
+					i+=2
+				else:
+					i+=1
+				if n == 0:
+					return True
+			else:
+				i+=2
+		return n <= 0
+
+arr_should_be_good = [1,0,0,0,0,0,1]
+arr_no_good = [1,0,0,0,1]
+arr = [1,0,0,0,1,0,0]
+sol = Solution()
+print(sol.canPlaceFlowers(arr, 2))

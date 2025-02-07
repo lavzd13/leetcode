11823767fe8038b517a4b474dfa34500d8c10984
Lomke <lavzd13@gmail.com>
2025-02-07 15:59:38 +0100

    Fix

--- easy_difficulty/cpp/maxProfit/main.cpp
+++ easy_difficulty/cpp/maxProfit/main.cpp
@@ -11,7 +11,7 @@ class Solution {
 			int	max_profit = 0;
 			for (int i = 0; i < prices.size(); i++) {
 				if (prices[i] < min)
-					min = prices[i];
+					min = std::min(prices[i], min);
 				max_profit = max(prices[i] - min, max_profit);
 			}
 			return max_profit;

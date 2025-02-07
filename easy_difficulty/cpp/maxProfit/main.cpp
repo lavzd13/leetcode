88d28e5c03b6f0f5d72412b66d90da482ecd7054
Lomke <lavzd13@gmail.com>
2025-02-07 15:58:46 +0100

    Added one exer.

---
+++ easy_difficulty/cpp/maxProfit/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+	public:
+		int maxProfit(vector<int>& prices) {
+			int	min = 99999999;
+			int	max_profit = 0;
+			for (int i = 0; i < prices.size(); i++) {
+				if (prices[i] < min)
+					min = prices[i];
+				max_profit = max(prices[i] - min, max_profit);
+			}
+			return max_profit;
+		}
+};
+
+int main() {
+	Solution	sol;
+
+	vector<int> arr = {7,1,5,3,6,4};
+	cout << sol.maxProfit(arr) << endl;
+}
